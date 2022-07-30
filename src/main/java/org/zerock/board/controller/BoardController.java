3fb9574d5abee2a7203ab087b8e48ac7166b523b
nakta <nakta0511@gmail.com>
2022-07-30 19:07:47 +0900

    index, list web page

---
+++ src/main/java/org/zerock/board/controller/BoardController.java
@@ -0,0 +1,30 @@
+package org.zerock.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.service.BoardService;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board/")
+@Log4j2
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public String list(PageRequestDTO requestDTO, Model model) {
+
+        log.info("list........." + requestDTO);
+
+        model.addAttribute("result", boardService.getList(requestDTO));
+
+        return "/board/list";
+    }
+}
