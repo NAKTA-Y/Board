1cb2ecaee948323802e797d5b61a903fc6108ad8
nakta <nakta0511@gmail.com>
2022-07-30 20:03:45 +0900

    register web page

--- src/test/java/org/zerock/board/repository/MemberRepositoryTest.java
+++ src/test/java/org/zerock/board/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package org.zerock.board.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.board.entity.Member;
 
@@ -11,11 +12,13 @@ import java.util.stream.IntStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Rollback(value = false)
     public void insertMembersTest() {
 
         IntStream.rangeClosed(1, 100).forEach(i -> {
